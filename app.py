03d46baf3f8b1aae281cec053597843f57969cba
David J. Malan <malan@harvard.edu>
2021-12-08 21:44:21 -0500

    Removed exception handling

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
@@ -126,15 +125,3 @@ def register():
 def sell():
     """Sell shares of stock"""
     return apology("TODO")
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
